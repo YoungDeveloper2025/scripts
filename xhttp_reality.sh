9f34c98da015ce695798f3dc58e8c1a9c0684913
onedev <49806461+YoungDeveloper2025@users.noreply.github.com>
2026-09-14 17:09:43 +0330

    Update xhttp_reality.sh

--- xhttp_reality.sh
+++ xhttp_reality.sh
@@ -98,7 +98,7 @@ main() {
         "network": "xhttp",
         "security": "reality",
         "realitySettings": {
-          "target": "play.google.com:443",
+          "target": "play-apps-features.googleusercontent.com:443",
           "xver": 0,
           "serverNames": [
             "play.google.com"
@@ -141,7 +141,7 @@ EOF
     # 删除运行脚本
     rm -f tcp-wss.sh install-release.sh reality.sh 
     IP=$(getIP)
-    url="vless://${UUID}@${IP}:${PORT}?encryption=none&security=reality&sni=play.google.com&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&allowInsecure=1&type=xhttp&mode=auto#$ISP"
+    url="vless://${UUID}@${IP}:${PORT}?encryption=none&security=reality&sni=play-apps-features.googleusercontent.com&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&allowInsecure=1&type=xhttp&mode=auto#$ISP"
         
     echo -e "\n\e[1;32mxhttp-reality 安装成功\033[0m\n"
     echo -e "\e[1;32m${url}\033[0m\n"
